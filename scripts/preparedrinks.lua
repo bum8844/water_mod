f4d2ae22f6072d9508e0bbcbc0f6c66079b00609
bum8844 <60079448+bum8844@users.noreply.github.com>
2021-08-20 13:01:14 +0900

    Update preparedrinks.lua
    코드복구

--- scripts/preparedrinks.lua
+++ scripts/preparedrinks.lua
@@ -1,125 +1,429 @@
-
-local function MakePreparedDrink(data)
-	local foodassets =
+local drinks =
+{
+	-- 조합법이 잘못되면 나오는 결과물
+	
+	cup_garbage = 
 	{
-		Asset("ANIM", "anim/kettle_drink.zip"),
-		Asset("INV_IMAGE", data.name),
-	}
+		test = function(boilier, name, tags) return true end,
+		priority = -2,
+		health = 0,
+		hunger = 0,
+		sanity = 0,
+		thirst = TUNING.HYDRATION_TINY,
+		cooktime = TUNING.INCORRECT_BOIL,
+		--wet_prefix = STRINGS.WET_PREFIX.WETGOOP,
+        --floater = {"small", nil, nil},
+	},
+	
+	-- 기본 물(이 외에 음료에는 얼음을 넣으면 찬음료 변경)
+	-- 다른 차를 만들 경우 같은 아이템을 2개 넣어야함
+	cup_water =
+	{
+		priority = 0,
+		health = TUNING.HEALING_SMALL,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = 0,
+		thirst = TUNING.HYDRATION_TINY,
+		cooktime = TUNING.KETTLE_WATER,
+	},
+	
+	-- 과일차 종류
+	
+	cup_fruit =
+	{
+		test = function(boilier, name, tags) return tags.fruit and tags.fruit >= 1.5 and not tags.meat and not tags.egg end,
+		priority = 0,
+		health = TUNING.HEALING_MEDSMALL,
+		hunger = TUNING.DRINK_CALORIES ,
+		sanity = TUNING.SANITY_TINY,
+		thirst = TUNING.HYDRATION_MEDSMALL,
+		cooktime = TUNING.KETTLE_FRUIT,
+	},
+	
+	cup_berry =
+	{
+		test = function(boilier, name, tags) return (( names.berries or 0 ) + ( names.berries_juicy or 0 ) >= 2) and not tags.meat and not tags.egg end,
+		priority = 1,
+		health = TUNING.HEALING_MEDSMALL,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = TUNING.SANITY_MED,
+		thirst = TUNING.HYDRATION_LARGE,
+		cooktime = TUNING.KETTLE_FRUIT,
+	},
 
-	if data.overridebuild then
-        table.insert(foodassets, Asset("ANIM", "anim/"..data.overridebuild..".zip"))
-	end
-
-    local foodprefabs = prefabs
-    if data.prefabs ~= nil then
-        foodprefabs = shallowcopy(prefabs)
-        for i, v in ipairs(data.prefabs) do
-            if not table.contains(foodprefabs, v) then
-                table.insert(foodprefabs, v)
+	cup_pomegranate =
+	{
+		test = function(boilier, name, tags) return names.pomegranate and names.pomegranate >= 2 and not tags.meat and not tags.egg end,
+		priority = 1,
+		health = TUNING.HEALING_MEDLARGE,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = TUNING.SANITY_MED,
+		thirst = TUNING.HYDRATION_LARGE,
+		cooktime = TUNING.KETTLE_FRUIT,
+	},
+	
+	cup_banana =
+	{
+		test = function(boilier, name, tags) return names.cave_banana and names.cave_banana >= 2 and tags.frozen and tags.frozen >= 1 and not tags.meat and not tags.egg end,
+		priority = 1,
+		health = TUNING.HEALING_MEDSMALL,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = TUNING.SANITY_MEDLARGE,
+		thirst = TUNING.HYDRATION_LARGE,
+		cooktime = TUNING.KETTLE_FRUIT,
+	},
+	
+	cup_fig =
+	{
+		test = function(boilier, name, tags) return names.fig and names.fig >= 2 and not tags.meat and not tags.egg end,
+		priority = 1,
+		health = TUNING.HEALING_MED,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = TUNING.SANITY_LARGE,
+		thirst = TUNING.HYDRATION_LARGE,
+		cooktime = TUNING.KETTLE_FRUIT,
+	},
+	
+	cup_dragonfruit =
+	{
+		test = function(boilier, name, tags) return names.dragonfruit and names.dragonfruit >= 2 and not tags.meat and not tags.egg end,
+		priority = 1,
+		health = TUNING.HEALING_HUGE,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = TUNING.SANITY_MED ,
+		thirst = TUNING.HYDRATION_HUGE,
+		cooktime = TUNING.KETTLE_FRUIT,
+	},
+	
+	-- 일시적 빛나는 효과
+	cup_wormlight =
+	{
+		test = function(boilier, name, tags) return (( names.wormlight or 0 ) + ( names.wormlight_lesser or 0) >= 2) and tags.frozen and tags.frozen >= 1 and not tags.meat and not tags.egg end,
+		priority = 1,
+		health = TUNING.HEALING_MED,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = TUNING.SANITY_TINY,
+		thirst = TUNING.HYDRATION_HUGE,
+		cooktime = TUNING.KETTLE_LUXURY_GOODS,
+        prefabs = { "wormlight_light_greater" },
+		oneat_desc = STRINGS.UI.COOKBOOK.FOOD_EFFECTS_GLOW,
+        oneatenfn = function(inst, eater)
+            --see wormlight.lua for original code
+            if eater.wormlight ~= nil then
+                if eater.wormlight.prefab == "wormlight_light_greater" then
+                    eater.wormlight.components.spell.lifetime = 0
+                    eater.wormlight.components.spell:ResumeSpell()
+                    return
+                else
+                    eater.wormlight.components.spell:OnFinish()
+                end
             end
-        end
-    end
 
-    local function DisplayNameFn(inst)
-        return subfmt(STRINGS.NAMES[data.spice.."_FOOD"], { food = STRINGS.NAMES[string.upper(data.basename)] })
-    end
-
-    local function fn()
-        local inst = CreateEntity()
-
-        inst.entity:AddTransform()
-        inst.entity:AddAnimState()
-        inst.entity:AddNetwork()
-
-        MakeInventoryPhysics(inst)
-
-		local food_symbol_build = nil
-
-		inst.AnimState:SetBuild(data.overridebuild or "kettle_drink")
-		inst.AnimState:SetBank("kettle_drink")
-
-        inst.AnimState:PlayAnimation("idle")
-        inst.AnimState:OverrideSymbol("swap_drink", data.overridebuild or "kettle_drink", data.basename or data.name)
-
-        inst:AddTag("drink")
-        if data.tags ~= nil then
-            for i,v in pairs(data.tags) do
-                inst:AddTag(v)
+            local light = SpawnPrefab("wormlight_light_greater")
+            light.components.spell:SetTarget(eater)
+            if light:IsValid() then
+                if light.components.spell.target == nil then
+                    light:Remove()
+                else
+                    light.components.spell:StartSpell()
+                end
             end
-        end
-
-        if data.basename ~= nil then
-            inst:SetPrefabNameOverride(data.basename)
-            if data.spice ~= nil then
-                inst.displaynamefn = DisplayNameFn
+        end,
+	},
+	
+	-- 일시적 겉는 속도 증가[추가해야함]
+	cup_coffee =
+	{
+		test = function(boilier, name, tags) return (( names.caffeinberry_bean_cooked or 0 ) + ( names.kyno_coffeebeans_cooked or 0 ) >= 2) and not tags.meat and not tags.egg end,
+		priority = 1,
+		health = TUNING.HEALING_SMALL,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = TUNING.SANITY_MED,
+		thirst = TUNING.HYDRATION_SMALL,
+		cooktime = 25,
+	},
+	
+	-- 야채차 종류
+	cup_veggie =
+	{
+		test = function(boilier, name, tags) return tags.veggie and tags.veggie >= 1.5 and not tags.meat and not tags.egg end,
+		priority = 0,
+		health = TUNING.HEALING_SMALL,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = TUNING.SANITY_TINY,
+		thirst = TUNING.HYDRATION_MEDSMALL,
+		cooktime = TUNING.KETTLE_VEGGIE,
+	},
+	
+	cup_carrot =
+	{
+		test = function(boilier, name, tags) return names.carrot and names.carrot >= 2 and not tags.meat and not tags.egg end,
+		priority = 0,
+		health = TUNING.HEALING_MEDSMALL,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = TUNING.SANITY_SMALL,
+		thirst = TUNING.HYDRATION_LARGE,
+		cooktime = TUNING.KETTLE_VEGGIE,
+	},
+	
+	-- 선인장, 다육, 알로에는 무조건 이걸로 만들어지게
+	cup_cactus =
+	{
+		test = function(boilier, name, tags) return (( names.cactus_meat or 0 ) + ( names.aloe or 0 ) + ( names.kyno_aloe or 0 ) + ( names.succulent_picked or 0 ) >= 2)  and tags.frozen and tags.frozen >= 1 end,
+		priority = 0,
+		health = TUNING.HEALING_SMALL,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = TUNING.SANITY_MEDLARGE,
+		thirst = TUNING.HYDRATION_LARGE,
+		temperature = TUNING.COLD_FOOD_BONUS_TEMP,
+		temperatureduration = TUNING.FOOD_TEMP_AVERAGE,
+		cooktime = TUNING.KETTLE_VEGGIE,
+	},
+	
+	cup_mulled =
+	{
+		test = function(boilier, name, tags) return (( names.onion or 0 ) + ( names.garlic or 0 ) >= 1) and tags.sweetener and not tags.frozen and not tags.meat and not tags.egg end,
+		priority = 0,
+		health = TUNING.HEALING_MEDSMALL,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = TUNING.SANITY_TINY,
+		thirst = TUNING.HYDRATION_LARGE,
+		temperature = TUNING.HOT_FOOD_WARMING_THRESHOLD,
+		temperatureduration = TUNING.FOOD_TEMP_AVERAGE,
+		cooktime = TUNING.KETTLE_VEGGIE,
+	},
+	
+	-- 만드레이크 차
+	cup_ginseng =
+	{
+		test = function(boilier, name, tags) return names.mandrake and not tags.meat and not tags.egg end,
+		priority = 4,
+		health = TUNING.HEALING_HUGE,
+		hunger = TUNING.DRINK_CALORIES_GINSENG,
+		sanity = TUNING.SANITY_SMALL,
+		thirst = TUNING.HYDRATION_HUGE,
+		cooktime = TUNING.KETTLE_LUXURY_GOODS,
+	},
+	
+	-- 버섯차 종류
+	cup_greencap =
+	{
+		test = function(boilier, name, tags) return names.green_cap and names.green_cap >= 2 and not tags.meat and not tags.egg end,
+		priority = 0,
+		health = 0,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = TUNING.SANITY_MED,
+		thirst = TUNING.HYDRATION_HUGE,
+		cooktime = TUNING.KETTLE_LUXURY_GOODS,
+		prefabs = { "sweettea_buff" },
+	    oneat_desc = STRINGS.UI.COOKBOOK.FOOD_EFFECTS_hot =_SANITY_REGEN,
+        oneatenfn = function(inst, eater)
+            if eater.components.debuffable ~= nil and eater.components.debuffable:IsEnabled() then
+                eater.components.debuffable:AddDebuff("sweettea_buff", "sweettea_buff")
             end
-        end
+        end,
+	},
+	
+	cup_redcap =
+	{
+		test = function(boilier, name, tags) return names.red_cap and names.red_cap >= 2 and not tags.meat and not tags.egg end,
+		priority = 0,
+		health = TUNING.HEALING_TINY,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = 0,
+		thirst = TUNING.HYDRATION_HUGE,
+		cooktime = TUNING.KETTLE_LUXURY_GOODS,
+        prefabs = { "healthregenbuff" },
+		oneat_desc = STRINGS.UI.COOKBOOK.FOOD_EFFECTS_HEALTH_REGEN,
+        oneatenfn = function(inst, eater)
+            if eater.components.debuffable ~= nil and eater.components.debuffable:IsEnabled() and
+                not (eater.components.health ~= nil and eater.components.health:IsDead()) and
+                not eater:HasTag("playerghost") then
+                eater.components.debuffable:AddDebuff("healthregenbuff", "healthregenbuff")
+            end
+        end,
+	},
+	
+	-- 정신력 반전 버프(제한시간)
+	cup_bluecap =
+	{
+		test = function(boilier, name, tags) return names.blue_cap and names.blue_cap >= 2 and not tags.meat and not tags.egg end,
+		priority = 0,
+		health = HEALING_MED,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = TUNING.SANITY_SMALL,
+		thirst = TUNING.HYDRATION_HUGE,
+		cooktime = TUNING.KETTLE_LUXURY_GOODS,
+	},
+	
+	cup_mooncap =
+	{
+		test = function(boilier, name, tags) return names.moon_cap and names.moon_cap >= 2 and not tags.meat and not tags.egg end,
+		priority = 0,
+		health = 0,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = TUNING.SANITY_SMALL,
+		thirst = TUNING.HYDRATION_HUGE,
+		cooktime = TUNING.KETTLE_LUXURY_GOODS,
+		prefabs = { "buff_sleepresistance" },
+        oneat_desc = STRINGS.UI.COOKBOOK.FOOD_EFFECTS_SLEEP_RESISTANCE,
+        oneatenfn = function(inst, eater)
+            if eater.components.grogginess ~= nil and
+                    (eater.components.debuffable ~= nil and eater.components.debuffable:IsEnabled()) and
+                    not (eater.components.health ~= nil and eater.components.health:IsDead()) and
+                    not eater:HasTag("playerghost") then
 
-        if data.floater ~= nil then
-            MakeInventoryFloatable(inst, data.floater[1], data.floater[2], data.floater[3])
-        else
-            MakeInventoryFloatable(inst)
-        end
+                if eater.components.grogginess ~= nil then
+                    eater.components.grogginess:ResetGrogginess()
+                end
 
-        inst.entity:SetPristine()
+                if eater.components.debuffable ~= nil and eater.components.debuffable:IsEnabled() then
+                    eater.components.debuffable:AddDebuff("shroomsleepresist", "buff_sleepresistance")
+                end
+            end
+        end,
+	},
+	
+	-- 잎&꽃차 종류
+	
+	cup_greentea =
+	{
+		test = function(boilier, name, tags) return names.tealeaves and names.tealeaves >= 2 and not tags.meat and not tags.egg end,
+		priority = 1,
+		health = TUNING.HEALING_MEDSMALL,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = SANITY_MEDLARGE,
+		thirst = TUNING.HYDRATION_MED,
+		cooktime = TUNING.KETTLE_TEA,
+	},
+	
+	-- 녹차 건조대 말린것
+	cup_blacktea =
+	{
+		test = function(boilier, name, tags) return names.tealeaves_dried and names.tealeaves_dried >= 2 and not tags.frozen and not tags.meat and not tags.egg end,
+		priority = 1,
+		health = HEALING_MEDSMALL,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = SANITY_MEDLARGE,
+		thirst = TUNING.HYDRATION_MED,
+		temperature = TUNING.HOT_FOOD_BONUS_TEMP,
+		temperatureduration = TUNING.FOOD_TEMP_LONG,
+		cooktime = TUNING.KETTLE_TEA,
+	},
+	
+	cup_blacktea_iced =
+	{
+		test = function(boilier, name, tags) return names.tealeaves_dried and names.tealeaves_dried >= 2 and tags.frozen and tags.frozen >= 1 and not tags.meat and not tags.egg end,
+		priority = 1,
+		health = HEALING_MEDSMALL,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = SANITY_MEDLARGE,
+		thirst = TUNING.HYDRATION_MED,
+		temperature = TUNING.COLD_FOOD_BONUS_TEMP,
+		temperatureduration = TUNING.FOOD_TEMP_LONG,
+		cooktime = TUNING.KETTLE_TEA,
+	},
+	
+	-- 동굴 고사리
+	cup_fuer =
+	{
+		test = function(boilier, name, tags) return names.foliage and names.foliage >= 2 and not tags.meat and not tags.egg end,
+		priority = 1,
+		health = HEALING_MED,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = SANITY_LARGE,
+		thirst = TUNING.HYDRATION_MED,
+		cooktime = TUNING.KETTLE_TEA,
+	},
+	
+	-- 꽃을 섞으면 나오는 결과물
+	cup_mixflower =
+	{
+		test = function(boilier, name, tags) return tags.decoration and tags.decoration >= 2 and tags.meat and not tags.egg end,
+		priority = 0,
+		health = HEALING_MEDSMALL,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = SANITY_SMALL,
+		thirst = TUNING.HYDRATION_MEDSMALL,
+		cooktime = TUNING.KETTLE_DECORATION,
+	},
+	
+	-- 일반 꽃잎
+	cup_hibiscus =
+	{
+		test = function(boilier, name, tags) return (( names.petals or 0 ) + ( names.moon_tree_blossom or 0 ) >= 2) and not tags.meat and not tags.egg end,
+		priority = 1,
+		health = HEALING_MEDSMALL,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = SANITY_MEDLARGE,
+		thirst = TUNING.HYDRATION_MED,
+		cooktime = TUNING.KETTLE_DECORATION,
+	},
+	
+	-- 악마 꽃잎(몬스터태그 확인해서 효과주기)
+	cup_bitter =
+	{
+		test = function(boilier, name, tags) return (( tags.monster or 0 ) + ( names.petals_evil or 0 )) >= 1.5) end,
+		priority = 2,
+            --if eater:HasTag("monster") and eater:HasTag("player") then
+		       --eater.components.health:DoDelta(37)
+		       --eater.components.sanity:DoDelta(25)
+            --end,
+	        --if not eater:HasTag("monster") and eater:HasTag("player") then
+		        --eater.components.health:DoDelta(-10)
+		        --eater.components.sanity:DoDelta(-20)
+		        --eater.components.hunger:DoDelta(-10)
+            --end,
+		thirst = TUNING.HYDRATION_HUGE,
+		cooktime = TUNING.KETTLE_MONSTER,
+	},
 
-        if not TheWorld.ismastersim then
-            return inst
-        end
+	-- 선인장 꽃잎
+	cup_cactusflower =
+	{
+		test = function(boilier, name, tags) return names.cactus_flower and names.cactus_flower >= 2 and not tags.meat and not tags.egg end,
+		priority = 1,
+		health = HEALING_MEDSMALL,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = SANITY_LARGE,
+		thirst = TUNING.HYDRATION_LARGE,
+		cooktime = TUNING.KETTLE_DECORATION,
+	},
+	
+	cup_lotusflower =
+	{
+		test = function(boilier, name, tags) return (( names.lotus_flower or 0 ) + ( names.kyno_lotus or 0 ) >= 2) and not tags.meat and not tags.egg end,
+		priority = 1,
+		health = HEALING_SMALL,
+		hunger = TUNING.DRINK_CALORIES,
+		sanity = SANITY_LARGE,
+		thirst = TUNING.HYDRATION_LARGE,
+		cooktime = TUNING.KETTLE_DECORATION,
+	},
+	
+	--일시적으로 유령으로 만드는 차[추가해야함]
+	cup_suspicious_hibiscus =
+	{
+		test = function(boilier, name, tags) return names.forgetmelots and names.firenettles and names.tillweeds) end,
+		priority = 2,
+		health = 0,
+		hunger = 0,
+		sanity = 0,
+		thirst = 0,
+		cooktime = TUNING.KETTLE_ABI,
+	},
+	
+}
 
-		inst.food_symbol_build = food_symbol_build or data.overridebuild
+for k, v in pairs(drinks) do
+    v.name = k
+    v.weight = v.weight or 1
+    v.priority = v.priority or 0
 
-        inst:AddComponent("edible")
-        inst.components.edible.healthvalue = data.health
-        inst.components.edible.hungervalue = data.hunger
-        inst.components.edible.foodtype = data.foodtype or FOODTYPE.GENERIC
-        inst.components.edible.secondaryfoodtype = data.secondaryfoodtype or nil
-        inst.components.edible.sanityvalue = data.sanity or 0
-        inst.components.edible.temperaturedelta = data.temperature or 0
-        inst.components.edible.temperatureduration = data.temperatureduration or 0
-        inst.components.edible.nochill = data.nochill or nil
-        inst.components.edible.spice = data.spice
-        inst.components.edible:SetOnEatenFn(data.oneatenfn)
-
-        inst:AddComponent("inspectable")
-        inst.wet_prefix = data.wet_prefix
-
-        inst:AddComponent("inventoryitem")
-
-        if spicename ~= nil then
-            inst.components.inventoryitem:ChangeImageName(spicename.."_over")
-        elseif data.basename ~= nil then
-            inst.components.inventoryitem:ChangeImageName(data.basename)
-        end
-
-        inst:AddComponent("stackable")
-        inst.components.stackable.maxsize = TUNING.STACK_SIZE_SMALLITEM
-
-
-        MakeSmallBurnable(inst)
-        MakeSmallPropagator(inst)
-        MakeHauntableLaunchAndPerish(inst)
-        ---------------------
-
-        ------------------------------------------------
-        inst:AddComponent("tradable")
-
-        ------------------------------------------------
-
-        return inst
-    end
-
-    return Prefab(data.name, fn, foodassets, foodprefabs)
+	v.cupsjournal_category = "kettles"
+	--v.cookbook_category = "cookpot"
 end
 
-local prefs = {}
+return drinks
 
-for k, v in pairs(require("preparedrinks")) do
-    table.insert(prefs, MakePreparedDrink(v))
-end
-
-for k, v in pairs(require("prepareagedrinks")) do
-    table.insert(prefs, MakePreparedDrink(v))
-end
-
-return unpack(prefs)
+-- caffeinberry caffeinberry_bean caffeinberry_bean_cooked
