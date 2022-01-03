f808acf03c07316a623963116d20665c0a30c69e
AFS Co. Ltd <qkrcotlr1@gmail.com>
2022-01-03 16:39:07 +0900

    Update modmain.lua
    PrefabFiles 정렬

--- modmain.lua
+++ modmain.lua
@@ -9,23 +9,21 @@ local CUSTOM_RECIPETABS = _G.CUSTOM_RECIPETABS
 
 PrefabFiles =
 {
- "bucket",
- "bucketfull",
- "bucketice",
- "cup",
- "preparedrink",
- "barrel",
- "brewery",
- "campkettle",
- "kettle",
- "portablekettle",
- "desalinator",
- "well_drilling",
- "tea_tree",
- "tea_seed",
- "caffeinberry_bean",
- "caffeinberry",
- "well",
+	"barrel",
+	"brewery",
+	"bucket",
+	"bucketfull",
+	"caffeinberry",
+	"campkettle",
+	"cup",
+	"desalinator",
+	"kettle",
+	"portablekettle",
+	"preparedrink",
+	"tea_tree",
+	"tea_seed",
+	"well",
+	"well_drilling",
 }
 
 Assets = {
