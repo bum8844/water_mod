7f2bb5a77a56ac5a56d843a1a13d0b8e1934cac3
bum8844 <bum8844@gmail.com>
2022-01-04 17:39:55 +0900

    배럴, 준비된 음료 컴포넌트작업
    배럴, 준비된 음료 컴포넌트작업

--- scripts/prefabs/barrel.lua
+++ scripts/prefabs/barrel.lua
@@ -11,89 +11,6 @@ local prefabs =
 	"collapse_small",
 }
 
---[[local function ShouldAcceptItem(inst, item, giver)
-	if item:HasTag("clean") or item:HasTag("fil_bucket") or item:HasTag("fil_bottle") or item:HasTag("fil_cup") then
-		return true
-	end
-end
-
-local function OnRefuseItem(inst, giver, item)
-	if item:HasTag("dirty") then
-		giver.components.talker:Say("I can't put it in because the water is dirty.")
-	elseif item:HasTag("salt") then
-		giver.components.talker:Say("I can't put it in because the water is salty.")
-	elseif inst._barrel_waterlevel == 0 then
-		giver.components.talker:Say("It's empty.")
-	else
-		giver.components.talker:Say("Can't get water with this.")
-	end
-end
-
-local function OnBackEmptyitem(inst, giver, item)
-	if item:HasTag("fil_bucket") then
-		giver.components.inventory:GiveItem(SpawnPrefab("bucket"))
-	elseif item:HasTag("fil_cup") then
-		giver.components.inventory:GiveItem(SpawnPrefab("cup"))
-	else
-		giver.components.inventory:GiveItem(SpawnPrefab("messagebottleempty"))
-	end
-	if inst._barrel_waterlevel == 0 then
-		giver.components.talker:Say("It's empty.")
-	else
-		giver.components.talker:Say("It's too little to get it!")
-	end
-end
-
-local function OnBackFullitem(inst, giver, item)
-	if item:HasTag("bucket") then
-		giver.components.inventory:GiveItem(SpawnPrefab("bucket_full"))
-	elseif item:HasTag("preparedrink_cup") then
-		giver.components.inventory:GiveItem(SpawnPrefab("cup_water"))
-	else
-		giver.components.inventory:GiveItem(SpawnPrefab("bottle_water"))
-	end
-	giver.components.talker:Say("There's too much water...")
-end
-
-local function OnGetItemFromPlayer(inst, giver, item)
-	local sum = inst._barrel_waterlevel
-	if sum >= 10 and item:HasTag("fil_bucket") then
-		inst._barrel_waterlevel = sum - 10
-		giver.components.inventory:GiveItem(SpawnPrefab("bucket_full"))
-	elseif sum >= 5 and item:HasTag("fil_bottle") then
-		inst._barrel_waterlevel = sum - 5
-		giver.components.inventory:GiveItem(SpawnPrefab("bottle_water"))
-	elseif sum >= 1 and item:HasTag("fil_cup") then
-		inst._barrel_waterlevel = sum - 1
-		giver.components.inventory:GiveItem(SpawnPrefab("cup_water"))
-	elseif item:HasTag("bucket") then
-		if sum + 10 > inst._barrel_waterlevel_max then
-			OnBackFullitem(inst, giver, item)
-		else
-			inst._barrel_waterlevel = sum + 10
-			giver.components.inventory:GiveItem(SpawnPrefab("bucket"))
-		end
-	elseif item:HasTag("preparedrink_bottle") then
-		if sum + 5 > inst._barrel_waterlevel_max then
-			OnBackFullitem(inst, giver, item)
-		else
-			inst._barrel_waterlevel = sum + 5
-			giver.components.inventory:GiveItem(SpawnPrefab("messagebottleempty"))
-		end
-	elseif item:HasTag("preparedrink_cup") then
-		if sum + 1 > inst._barrel_waterlevel_max then
-			OnBackFullitem(inst, giver, item)
-		else
-			inst._barrel_waterlevel = sum + 1
-			giver.components.inventory:GiveItem(SpawnPrefab("cup"))
-		end
-		WaterLevelChange(inst, giver, item)
-	else
-		OnBackEmptyitem(inst, giver, item)
-	end
-end
-]]--
-
 local function WaterLevelChange(inst, giver, item)
 	if item:HasTag("fil_bucket") or item:HasTag("bucket") then
 		inst.SoundEmitter:PlaySound("dontstarve/creatures/pengull/splash")
@@ -102,7 +19,7 @@ local function WaterLevelChange(inst, giver, item)
 	else
 		inst.SoundEmitter:PlaySound("turnoftides/common/together/water/emerge/small")
 	end
-	local sum = inst._barrel_waterlevel
+	local sum = 
 	inst.AnimState:OverrideSymbol("swap","barrel_meter_water", tostring(sum))
 end
 
@@ -137,11 +54,37 @@ local function onload(inst, data)
     end
 end
 
-local function onloadpostpass(inst, newents, data)
-	if data.waterlevel ~= 0 then
-	local sum = data.waterlevel
+--[[local function onloadpostpass(inst, newents, data)
+	if data.waterlevel ~= nil or 0 then
+	local sum = 
 	inst.AnimState:OverrideSymbol("swap","barrel_meter_water", tostring(sum))
 	end
+end]]
+
+local function OnChargedFn(inst)
+    --[[if inst.task ~= nil then
+        inst.task:Cancel()
+        inst.task = nil
+        inst.components.resistance:SetOnResistDamageFn(OnResistDamage)
+    end
+    for i, v in ipairs(RESISTANCES) do
+        inst.components.resistance:AddResistance(v)
+    end]]
+end
+
+local function nofuel(inst)
+    --[[inst.components.cooldown.onchargedfn = nil
+    inst.components.cooldown:FinishCharging()]]
+end
+
+local function ontakefuel(inst)
+    --[[if inst.components.equippable:IsEquipped() and
+        not inst.components.fueled:IsEmpty() and
+        inst.components.cooldown.onchargedfn == nil then
+        inst.components.cooldown.onchargedfn = OnChargedFn
+        inst.components.cooldown:StartCharging(TUNING.ARMOR_SKELETON_FIRST_COOLDOWN)
+    end]]
+    inst.SoundEmitter:PlaySound("dontstarve/common/nightmareAddFuel")
 end
 
 local function fn()
@@ -159,7 +102,7 @@ local function fn()
     MakeObstaclePhysics(inst, .1)
 	
     inst.AnimState:SetBuild("barrel")
-    inst.AnimState:SetBank("barrel")
+    inst.AnimState:SetBank("waterbarrel")
     inst.AnimState:PlayAnimation("idle")
 	inst.AnimState:OverrideSymbol("swap","barrel_meter_water", "0")
 	
@@ -172,9 +115,6 @@ local function fn()
         return inst
     end
 	
-	--inst._barrel_waterlevel = 0
-	--inst._barrel_waterlevel_max = 20
-	
 	inst:AddComponent("lootdropper")
     inst:AddComponent("inspectable")
 	
@@ -184,28 +124,27 @@ local function fn()
 	inst.components.workable:SetOnFinishCallback(onhammered)
 	inst.components.workable:SetOnWorkCallback(onhit)
 	
+    inst:AddComponent("fueled")
+    inst.components.fueled.fueltype = "WATER"
+    inst.components.fueled:InitializeFuelLevel(0)
+    inst.components.fueled:SetDepletedFn(nofuel)
+    inst.components.fueled:SetTakeFuelFn(ontakefuel)
+    inst.components.fueled.accepting = true
+	print(inst.components.fueled.fueltype)
+	
 	inst:ListenForEvent("onbuilt", onbuilt)
 	
 	MakeHauntableWork(inst)
 	
 	MakeMediumBurnable(inst, nil, nil, true)
     MakeSmallPropagator(inst)
-	
-	inst:AddComponent("waterlevel")
-	inst.components.waterlevel:SetLevel(0)
-	inst.components.waterlevel:SetMaxLevel(20)
-	inst.components.waterlevel:SetLevelCallback(WaterLevelChange)
-	--[[inst:AddComponent("trader")
-	inst.components.trader:SetAcceptTest(ShouldAcceptItem)
-	inst.components.trader.onaccept = OnGetItemFromPlayer
-	inst.components.trader.onrefuse = OnRefuseItem]]--
 
     inst.OnSave = onsave
     inst.OnLoad = onload
-	inst.OnLoadPostPass = onloadpostpass
+	--inst.OnLoadPostPass = onloadpostpass
 	
     return inst
 end
 
 return Prefab("barrel", fn, assets),
-MakePlacer("barrel_placer", "barrel", "barrel", "idle")
+MakePlacer("barrel_placer", "waterbarrel", "barrel", "idle")
