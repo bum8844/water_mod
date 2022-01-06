7c9a82e7b4ecb740c3a5606270fdfddd1ebcd72f
bum8844 <bum8844@gmail.com>
2022-01-06 21:43:49 +0900

    새로운 컴포넌트 작업
    새로운 컴포넌트 작업

--- scripts/components/drinkvalue.lua
+++ scripts/components/drinkvalue.lua
@@ -1,14 +1,46 @@
+local function onwatertype(self, watertype, old_watertype)
+    if old_watertype ~= nil then
+        self.inst:RemoveTag(old_watertype.."_drinkvalue")
+    end
+    if watertype ~= nil then
+        self.inst:AddTag(watertype.."_drinkvalue")
+    end
+end
 
 local DrinkValue = Class(function(self, inst)
     self.inst = inst
-	self.levelvalue = 0
+    self.drinkvalue = 1
+    self.watertype = nil
+    self.ontaken = nil
 end,
 nil,
 {
+    drinkvalue = ondrinkvalue,
 })
 
-function DrinkValue:SetVelue(num)
-	self.levelvalue = num
+function DrinkValue:SetWaterType(types)
+	self.watertype = types
+end
+
+function DrinkValue:SetDrinkValue(num)
+	self.drinkvalue = num
+end
+
+function DrinkValue:OnRemoveFromEntity()
+    if self.watertype ~= nil then
+        self.inst:RemoveTag(self.watertype.."_drinkvalue")
+    end
+end
+
+function DrinkValue:SetOnTakenFn(fn)
+    self.ontaken = fn
+end
+
+function DrinkValue:Taken(taker)
+    self.inst:PushEvent("drinkvaluetaken", {taker = taker})
+    if self.ontaken then
+        self.ontaken(self.inst, taker)
+    end
 end
 
 return DrinkValue
