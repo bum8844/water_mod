9631fab03e1f3df1a43ff08c0871bdfb0cf723e2
bum8844 <bum8844@gmail.com>
2022-03-08 23:39:04 +0900

    위젯 수정(작동 잘 안됨...)
    위젯 수정(작동 잘 안됨...) 분석하고 수정해야함...

--- scripts/widgets/thirstbadge_statusdisplays.lua
+++ scripts/widgets/thirstbadge_statusdisplays.lua
@@ -1,14 +1,18 @@
-local ThirstBadge      = require "widgets/thirstbadge"
-
 local function thirstbadge_statusdisplays(self)
 
 	_SetGhostMode = self.SetGhostMode
+	_ShowStatusNumbers = self.ShowStatusNumbers
+	_HideStatusNumbers = self.HideStatusNumbers
+	_AddWereness = self.AddWereness
+	_SetWereMode = self.SetWereMode
 
+	local ThirstBadge = require "widgets/thirstbadge"
+	
     self.waterstomach = self:AddChild(self.owner.CreateThirstBadge ~= nil and self.owner.CreateThirstBadge(self.owner) or ThirstBadge(self.owner))
     self.waterstomach:SetPosition(-60, 20, 0)
     self.onthirstdelta = nil
 
-	local function OnSetPlayerMode_Water(self)
+	local function OnSetPlayerMode_Water(inst, self)
 		if self.onthirstdelta == nil then
 	        self.onthirstdelta = function(owner, data) self:thirstDelta(data) end
 	        self.inst:ListenForEvent("thirstdelta", self.onthirstdelta, self.owner)
@@ -16,7 +20,7 @@ local function thirstbadge_statusdisplays(self)
 	    end
 	end
 
-	local function OnSetGhostMode_Water(self)
+	local function OnSetGhostMode_Water(inst, self)
 	    if not self.isghostmode == not ghostmode then --force boolean
 	        return
 	    elseif ghostmode then
@@ -28,17 +32,64 @@ local function thirstbadge_statusdisplays(self)
 		end
 	end
 
-	function self.SetGhostMode(ghostmode, ...)
-	    if not self.isghostmode == not ghostmode then --force boolean
+	function self.ShowStatusNumbers()
+	    self.waterstomach.num:Show()
+		local result = ShowStatusNumbers()
+	    return result
+	end
+
+	function self.HideStatusNumbers()
+		self.waterstomach.num:Hide()
+		local result = _HideStatusNumbers()
+	    return result
+	end
+
+	function self.AddWereness()
+		if self.wereness == nil then
+			self.wereness:SetPosition(self.waterstomach:GetPosition())
+		end
+		local result = _AddWereness()
+
+		return result
+	end
+
+	function self.SetWereMode(weremode, nofx, ...)
+	    if self.isghostmode or self.wereness == nil then
 	        return
-	    elseif ghostmode then
+	    elseif weremode then
 	        self.waterstomach:Hide()
+	        self.wereness:SetPosition(self.waterstomach:GetPosition())
 	    else
 	        self.waterstomach:Show()
 	    end
-	    local result = _SetGhostMode(ghostmode, ...)
-	    self.modetask = self.inst:DoStaticTaskInTime(0, ghostmode and OnSetGhostMode_Water or OnSetPlayerMode_Water, self)
-		return result
+	    local result = _SetWereMode(self, weremode, nofx, ...)
+	    return result
+	end
+
+	function self.SetGhostMode(ghostmode, ...)
+		local old_isghostmode = self.isghostmode
+		self.modetask_water = nil
+
+	    if not self.isghostmode == not ghostmode then
+	        return
+	    elseif ghostmode then
+        	self.isghostmode = true
+	        self.waterstomach:Hide()
+	    else
+        	self.isghostmode = nil
+	        self.waterstomach:Show()
+	    end
+
+	    if self.modetask_water ~= nil then
+	        self.modetask_water:Cancel()
+	    end
+
+	    self.modetask_water = self.inst:DoStaticTaskInTime(0, ghostmode and OnSetGhostMode_Water or OnSetPlayerMode_Water, self)
+
+	    self.isghostmode = old_isghostmode
+
+	    local result = _SetGhostMode(self, ghostmode, ...)
+	    return result
 	end
 
 	function self:SetThirstPercent(pct)
