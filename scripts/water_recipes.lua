d54817ef16e96de629cab1384c77703bfb3cbe7c
bum8844 <bum8844@gmail.com>
2022-04-21 16:05:09 +0900

    레시피 수정과 목마름 배지 색깔 수정
    레시피 수정과 목마름 배지 색깔 수정

--- scripts/water_recipes.lua
+++ scripts/water_recipes.lua
@@ -1,33 +1,32 @@
-local CUSTOM_RECIPETABS _G = GLOBAL
-local RECIPETABS = _G.RECIPETABS
+local _G = GLOBAL
 local TECH = _G.TECH
 local Ingredient = _G.Ingredient
-local MyRecipesTab = RECIPETABS.DRINKS_TAB
+local ModAtlas = "images/tea_inventoryitem.xml"
 
 --local craft_bag=GetModConfigData("craft_bag")
 
 for k, v in pairs(GLOBAL.AllRecipes) do
 	if v.name == "fertilizer" then
-		v.ingredients = {GLOBAL.Ingredient("poop", 3), GLOBAL.Ingredient("boneshard", 2), GLOBAL.Ingredient("bucket", 1, "images/tea_inventoryitem.xml","bucket_empty.tex")}
+		v.ingredients = {GLOBAL.Ingredient("poop", 3), GLOBAL.Ingredient("boneshard", 2), GLOBAL.Ingredient("bucket", 1, ModAtlas,"bucket_empty.tex")}
 	end
 end
 
-AddRecipe("cup",{Ingredient("flint",3)},RECIPETABS.DRINKS_TAB,TECH.NONE,nil,nil,nil,nil,nil,"images/tea_inventoryitem.xml","cup_empty.tex")
+AddRecipe2("cup",{Ingredient("flint",3)},TECH.NONE,{atlas = ModAtlas, image = "cup_empty.tex"},{"REFINE"})
 
-AddRecipe("bucket",{Ingredient("log",4)},RECIPETABS.DRINKS_TAB,TECH.NONE,nil,nil,nil,nil,nil,"images/tea_inventoryitem.xml","bucket_empty.tex")
+AddRecipe2("bucket",{Ingredient("log",4)},TECH.NONE,{atlas = ModAtlas, image = "bucket_empty.tex"},{"REFINE","RAIN"})
 
-AddRecipe("barrel",{Ingredient("boards",2),Ingredient("rope",3)},RECIPETABS.DRINKS_TAB,TECH.SCIENCE_ONE,"barrel_placer",nil,nil,nil,nil,"images/tea_inventoryitem.xml","barrel.tex")
+AddRecipe2("barrel",{Ingredient("boards",2),Ingredient("rope",3)},TECH.SCIENCE_ONE,{placer = "barrel_placer", atlas = ModAtlas, image = "barrel.tex"},{"CONTAINERS"})
 
-AddRecipe("brewery",{Ingredient("boards",3),Ingredient("rope",3),Ingredient("cutstone",12),Ingredient("goldnugget",4)},RECIPETABS.DRINKS_TAB,TECH.SCIENCE_TWO,"brewery_placer",nil,nil,nil,nil,"images/tea_inventoryitem.xml","brewery.tex")
+AddRecipe2("brewery",{Ingredient("boards",3),Ingredient("rope",3),Ingredient("cutstone",12),Ingredient("goldnugget",4)},TECH.SCIENCE_TWO,{placer = "brewery_placer", atlas = ModAtlas, image = "brewery.tex"},{"COOKING"})
 
-AddRecipe("campkettle",{Ingredient("twigs",2),Ingredient("log",2),Ingredient("cutgrass",2)},RECIPETABS.DRINKS_TAB,TECH.NONE,"campkettle_placer",nil,nil,nil,nil,"images/tea_inventoryitem.xml","campkettle.tex")
+AddRecipe2("campkettle",{Ingredient("twigs",2),Ingredient("log",2),Ingredient("cutgrass",2)},TECH.NONE,{placer = "campkettle_placer", atlas = ModAtlas, image = "campkettle.tex"},{"COOKING","LIGHT"})
 
-AddRecipe("kettle",{Ingredient("twigs",6),Ingredient("rope",2),Ingredient("charcoal",2)},RECIPETABS.DRINKS_TAB,TECH.SCIENCE_ONE,"kettle_placer",nil,nil,nil,nil,"images/tea_inventoryitem.xml","kettle.tex")
+AddRecipe2("kettle",{Ingredient("twigs",6),Ingredient("rope",2),Ingredient("charcoal",2)},TECH.SCIENCE_ONE,{placer = "kettle_placer", atlas = ModAtlas, image = "kettle.tex"},{"COOKING"})
 
-AddRecipe("portablekettle_item",{Ingredient("transistor",2),Ingredient("cutstone",4),Ingredient("charcoal",3)},RECIPETABS.DRINKS_TAB,TECH.SCIENCE_TWO,nil,nil,nil,nil,nil,"images/tea_inventoryitem.xml","portablekettle_item.tex")
+AddRecipe2("portablekettle_item",{Ingredient("transistor",2),Ingredient("cutstone",4),Ingredient("charcoal",3)},TECH.SCIENCE_TWO,{atlas = ModAtlas, image = "portablekettle_item.tex"},{"COOKING"})
 
-AddRecipe("desalinator",{Ingredient("boards",12),Ingredient("rope",6),Ingredient("rocks",6),Ingredient("nitre",6),Ingredient("charcoal",6)},RECIPETABS.DRINKS_TAB,TECH.SCIENCE_TWO,"desalinator_placer",nil,nil,nil,nil,"images/tea_inventoryitem.xml","desalinator.tex")
+AddRecipe2("desalinator",{Ingredient("boards",12),Ingredient("rope",6),Ingredient("rocks",6),Ingredient("nitre",6),Ingredient("charcoal",6)},TECH.SCIENCE_TWO,{placer = "desalinator_placer", atlas = ModAtlas, image = "desalinator.tex"},{"CONTAINERS","GARDENING"})
 
-AddRecipe("well_drilling_item",{Ingredient("boards",2),Ingredient("cutstone",3),Ingredient("flint",4)},RECIPETABS.DRINKS_TAB,TECH.SCIENCE_TWO,nil,nil,nil,nil,nil,"images/tea_inventoryitem.xml","well_drilling_item.tex")
+AddRecipe2("well_drilling_item",{Ingredient("boards",2),Ingredient("cutstone",3),Ingredient("flint",4)},TECH.SCIENCE_TWO,{atlas = ModAtlas, image = "well_drilling_item.tex"},{"GARDENING"})
 
 GLOBAL.CONSTRUCTION_PLANS["hole"] = {Ingredient("boards",2),Ingredient("cutstone",6),Ingredient("hammer",1)}
