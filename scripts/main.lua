ce414d68b539b4f5bb425adeca7cb1b38be07542
bum8844 <60079448+bum8844@users.noreply.github.com>
2023-03-02 12:17:33 +0900

    비료관련 수정
    비료관련 수정

--- scripts/main.lua
+++ scripts/main.lua
@@ -16,10 +16,6 @@ modrequire("postinits/postinit")
 local drinks = require("prepareddrinks")
 local drinks_fermented = require("preparedageddrinks")
 
---AddFertilizerValue
-local FERTILIZERS = require("prefabs/fertilizer_nutrient_defs")
-FERTILIZERS.FERTILIZER_DEFS.spoiled_drink = {nutrients = TUNING.SPOILED_FOOD_NUTRIENTS}
-
 for k, recipe in pairs(drinks) do
 	AddCookerRecipe("kettle", recipe)
 	AddCookerRecipe("portablekettle", recipe)
