d1eaf8379791aa950ccc29a275b73e9bf941bb1e
bum8844 <60079448+bum8844@users.noreply.github.com>
2023-03-22 09:56:29 +0900

    파이프 관련 코드

--- scripts/prefabs/well_water_pipe.lua
+++ scripts/prefabs/well_water_pipe.lua
@@ -11,18 +11,18 @@ local prefabs =
 local function HideLayers(inst)
     local rays = {1, 2, 3}
     for i = 1, #rays, 1 do
-        inst.AnimState:Hide("joint"..i)
-        inst.AnimState:Hide("pipe"..i)
+        inst.AnimState:Hide("joint_"..i)
+        inst.AnimState:Hide("pipe_"..i)
     end
 end
 
 local function ShowRandomLayers(inst)
 	if not inst.jointLayerShown then
-		inst.jointLayerShown = "joint"..math.random(1, 3)
+		inst.jointLayerShown = "joint_"..math.random(1, 3)
 	end
 
 	if not inst.pipeLayerShown then
-		inst.pipeLayerShown = "pipe"..math.random(1, 3)
+		inst.pipeLayerShown = "pipe_"..math.random(1, 3)
 	end
 
 	inst.AnimState:Show(inst.jointLayerShown)
