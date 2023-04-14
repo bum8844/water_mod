9af45414f2220085be7e50f27ed0bb016f821142
AFS Co. Ltd <qkrcotlr1@gmail.com>
2023-04-15 03:34:37 +0900

    대사 템플릿 추가

---
+++ scripts/strings/speech_generic.lua
@@ -0,0 +1,155 @@
+return {
+	DESCRIBE =
+	{
+		ACTIONFAIL =
+		{
+			GIVE =
+			{
+				WELL_NOTEMPTY = ".",
+				WELL_BUSY = "아직 물동이가 다 올라오지 않았어.",
+			}
+		},
+
+		ANNOUNCE_DRUNK = "히끅... 나 취한 것 같아.",
+		ANNOUNCE_DRUNK_END = "좋아, 취기가 싹 사라졌어.",
+		ANNOUNCE_DRUNK_IMMUNITY = "이 정도로 날 취하게 할 수는 없어!",
+		ANNOUNCE_NOTCHARGE = "아직 젖이 나올 상태가 아니야.",
+		ANNOUNCE_NOTHIRSTSLEEP = "이대로 자면 말라죽을 거야!",
+		ANNOUNCE_NOTHIRSTSIESTA = "",
+		ANNOUNCE_SLEEP_END = "자고 나니까 좀 상쾌하네!",
+		ANNOUNCE_THIRST = "너무 목말라!",
+
+		DESCRIBE =
+		{
+			WATER_CLEAN_ICE = "",
+			WATER_DIRTY_ICE = "",
+
+			SPICE_CAFFEINPEPPER = "갈았더니 후추 향이 나네. 흥미로워.",
+
+			WELL_KIT = "",
+			WELL_DRILLING = "구멍을 파고 있는 중이야.",
+			WELL_DRILLING_ITEM = "물을 긷기 위해 우물을 파는 장치야.",
+			HOLE = "바닥이 보이지 않을 정도로 깊어.",
+			WELL = "",
+			WELL_SPRINKLER = "",
+
+			BUCKET_EMPTY = "물을 채울 수 있어. 물 말고 다른 걸 채워도 되고.",
+
+			TEA_TREE = "찻잎을 채취할 수 있는 나무야.",
+			DUG_TEA_TREE = "잎에서 풍부한 향이 느껴져.",
+			TEA_SEED = "차나무의 열매야!",
+			TEA_SEED_COOKED = "이런 행동은 하지 말았어야 했는데.",
+			TEA_SEED_SAPLING = "시간이 지나면 나무로 자랄 거야.",
+			TEALEAVES = "내가 마시던 차는 이렇게 안 생겼는데.",
+			TEALEAVES_DRIED = "이제 내가 아는 차의 모양이야.",
+
+			CAFFEINBERRY = "땅속에 단단히 뿌리내리고 있어.",
+			DUG_CAFFEINBERRY = "땅으로 옮겨줘야 해!",
+			CAFFEINBERRY_BEAN = "내가 아는 커피콩하고는 다르게 생겼는데.",
+			CAFFEINBERRY_BEAN_COOKED = "가열했더니 확실히 나아졌어.",
+
+			WATER_CLEAN = "바로 마셔도 괜찮겠어.",
+			WATER_DIRTY = "그냥 마시기에는 좀 위험하겠는데.",
+			WATER_SALTY = "염분 때문에 마시기 좋지 않아.",
+
+			GOOPYDRINK = "",
+			GARBAGE = "",
+			FRUITJUICY = "과일 향이 향긋해.",
+			BERRIES_JUICE = "베리에서 과학을 추출해 냈어.",
+			POMEGRANATE_JUICE = "",
+			BANANA_JUICE = "",
+			FIG_JUICE = "",
+			DRAGONJUICE = "",
+			GLOWBERRYJUICE = "",
+			CAFFEINBERRY_JUICE = "",
+
+			VEGGIE_TEA = "채소 풋내가 나는데.",
+			CARROT_TEA = "마실 만했으면 좋겠네.",
+			CACTUS_TEA = "",
+			MULLED = "",
+			GREENTEA = "이것도 차란 말이야?",
+			BLACKTEA = "우유가 조금 있으면 좋겠는데.",
+			BLACKTEA_ICED = "",
+			FUER = "",
+			MIXFLOWER = "",
+			HIBISCUSTEA = "",
+			CACTUSFLOWER_TEA = "",
+			LOTUSTEA = "",
+			SUSHIBISCUS = "",
+
+			SPOILED_DRINK = "",
+			SODA = "",
+			FRUITSODA = "",
+			LEMONLIMESODA = "",
+			COLA = "",
+			COLAQUANTUM = "",
+			CORN_BEER = "",
+			MADHU = "",
+			WINE = "",
+			NOBLEWINE = "",
+			SPARKLINGWINE = "",
+			GLOWBERRYWINE = "",
+			KUMIS = "",
+
+			DESALINATOR =
+			{
+				EMPTY = "",
+				PURIFY_LONG = "",
+				PURIFY_SHORT = "",
+				HASWATER = "",
+				BURNT = "",
+				HASSALT = "",
+			},
+
+			BARREL =
+			{
+				GENERIC = "",
+				BURNT = "",
+			},
+
+			BREWERY =
+			{
+				EMPTY = "",
+				BURNT = "",
+				FERMENTING_LONG = "",
+				FERMENTING_SHORT = "",
+				DONE = "",
+			},
+
+			CAMPKETTLE_ITEM = "",
+			CAMPKETTLE =
+			{
+				GENERIC = "",
+				BOILING_LONG = "",
+				BOILING_SHORT = "",
+				DONE = "",
+				STOP = "",
+			},
+
+			KETTLE =
+			{
+				EMPTY = "",
+				BURNT = "",
+				BOILING_LONG = "",
+				BOILING_SHORT = "",
+				DONE = "",
+				PURIFY_LONG = "",
+				PURIFY_SHORT = "",
+				HASWATER = "",
+			},
+
+			PORTABLEKETTLE_ITEM =
+			{
+				GENERIC = "",
+				EMPTY = "", 
+				BOILING_LONG = "",
+				BOILING_SHORT = "",
+				DONE = "",
+				PURIFY_LONG = "",
+				PURIFY_SHORT = "",
+				HASWATER = "",
+			}
+		}
+
+	}
+}
