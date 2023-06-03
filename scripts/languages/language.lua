04f3cab31094eddea3f408add5b521d68ccede86
AFS Co. Ltd <qkrcotlr1@gmail.com>
2023-06-04 08:58:40 +0900

    speech_generic 작업 완료
    현재 작업 완료된 파일:
    strings.lua
    speech_generic.lua

---
+++ scripts/languages/language.lua
@@ -0,0 +1,23 @@
+local vanilla_characters = {
+	"generic",
+	"walter",
+	"wanda",
+	"warly",
+	"wathgrithr",
+	"waxwell",
+	"webber",
+	"wendy",
+	"wickerbottom",
+	"willow",
+	"wolfgang",
+	"woodie",
+	"wormwood",
+	"wortox",
+	"wurt",
+	"wx78",
+}
+
+local function MergeTable( base, tbl )
+
+for _, characters in pairs(vanilla_characters) do
+
