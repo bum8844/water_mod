9f254de5ea674ea5a326c11e7860941a200341ad
bum8844 <60079448+bum8844@users.noreply.github.com>
2023-08-01 11:50:36 +0900

    태스트 해봐야함

--- scripts/prefabs/bucket.lua
+++ scripts/prefabs/bucket.lua
@@ -244,7 +244,7 @@ local function fn()
     inst.components.fuel.fuelvalue = TUNING.LARGE_FUEL
     
     inst:AddComponent("wateringtool")
-    inst.components.wateringtool:SetCanContainRain(true)
+    inst.components.wateringtool:SetCanCollectRainWater(true)
     inst.components.wateringtool.makeemptyfn = MakeFull
     inst.components.wateringtool.makefullfn = MakeEmpty
 
