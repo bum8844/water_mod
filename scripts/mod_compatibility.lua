354e17e940dc50ea4be81f085ef60bd82cc68210
bum8844 <60079448+bum8844@users.noreply.github.com>
2023-12-24 15:31:57 +0900

    향신료 관련 패치

--- scripts/mod_compatibility.lua
+++ scripts/mod_compatibility.lua
@@ -13,9 +13,6 @@ for k,mod_id in ipairs(GLOBAL.KnownModIndex:GetModsToLoad()) do
 		modrequire("modcompats/1467214795/ia_init")
 		modrequire("modcompats/1467214795/ia_init_prefab")
 	end 
-	if mod_id == "workshop-2039181790" then
-		modrequire("modcompats/2039181790/unc_water_spicedfoods_mod")
-	end
 	if mod_id == "workshop-1289779251" then 
 		modrequire("modcompats/1289779251/cherry_friendpits")
 	end
