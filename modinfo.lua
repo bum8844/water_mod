1b1153bf17a4ae6b927e8743d2313ce64510df6f
bum8844 <60079448+bum8844@users.noreply.github.com>
2024-04-30 19:55:53 +0900

    버전업

--- modinfo.lua
+++ modinfo.lua
@@ -1,5 +1,5 @@
 name 						= "Don't Starve: Dehydrated"
-version 					= "Alpha 1.2.6a"
+version 					= "Alpha 1.2.6b"
 local info_version = "Version "..version.."\n"
 
 description = info_version..[[
