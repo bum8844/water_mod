6e3a20bab60de8bd4a3673d5167ea462f84768f4
bum8844 <60079448+bum8844@users.noreply.github.com>
2024-05-18 10:31:41 +0900

    물펌프 색션 관련 작업을 위한 패치

--- scripts/components/steampressure.lua
+++ scripts/components/steampressure.lua
@@ -79,6 +79,42 @@ function SteamPressure:SetPressure(num)
 	self.curpressure = math.ceil(num*.5)
 end
 
+function SteamPressure:IsDepleted()
+	return self.depleted
+end
+
+function SteamPressure:IsEmptyPressure()
+    return self.curpressure <= 0
+end
+
+function SteamPressure:SetPressureSections(num)
+    self.pressuresection = num
+end
+
+function SteamPressure:IsSame()
+    return self.maxpressure == self.pressuresection
+end
+
+function SteamPressure:GetSection()
+    return self:IsSame() and math.floor(self:GetPressurePercent()* self.pressuresection) or math.floor(self:GetPressurePercent()* self.pressuresection)+1
+end
+
+function SteamPressure:GetPressureSection()
+    return self:IsEmptyPressure() and 0 or math.min(self:GetSection(), self.pressuresection)
+end
+
+function SteamPressure:IsLowPressure()
+	return self.curpressure / self.maxpressure <= .3
+end
+
+function SteamPressure:IsHighPressure()
+	return self.curpressure / self.maxpressure >= .7
+end
+
+function SteamPressure:GetPressurePercent()
+    return self.maxpressure > 0 and math.max(0, math.min(1, self.curpressure / self.maxpressure)) or 0
+end
+
 function SteamPressure:GetPressure()
     local time = self:IsDepleted() and self.delayed * 0.01 or (self.delayed * 0.25) * 0.01
     self.delay_left_time = time + GetTime()
@@ -154,42 +190,6 @@ function SteamPressure:LostPressure()
     end
 end
 
-function SteamPressure:IsDepleted()
-	return self.depleted
-end
-
-function SteamPressure:IsEmptyPressure()
-    return self.curpressure <= 0
-end
-
-function SteamPressure:SetPressureSections(num)
-    self.pressuresection = num
-end
-
-function SteamPressure:IsLowPressure()
-	return self.curpressure / self.maxpressure <= .3
-end
-
-function SteamPressure:IsHighPressure()
-	return self.curpressure / self.maxpressure >= .7
-end
-
-function SteamPressure:GetPressurePercent()
-    return self.maxpressure > 0 and math.max(0, math.min(1, self.curpressure / self.maxpressure)) or 0
-end
-
-function SteamPressure:IsSame()
-    return self.maxpressure == self.pressuresection
-end
-
-function SteamPressure:GetSection()
-    return self:IsSame() and math.floor(self:GetPressurePercent()* self.pressuresection) or math.floor(self:GetPressurePercent()* self.pressuresection)+1
-end
-
-function SteamPressure:GetPressureSection()
-    return self:IsEmptyPressure() and 0 or math.min(self:GetSection(), self.pressuresection)
-end
-
 function SteamPressure:OnSave()
 	local remainingtime = self.left_time ~= nil and self.left_time - GetTime() or 0
 	local remainingdelaytime = self.delay_left_time ~= nil and self.delay_left_time - GetTime() or 0
@@ -203,15 +203,22 @@ end
 
 function SteamPressure:OnLoad(data)
     if data ~= nil then
+    	local oldsection = self:GetPressureSection()
         self.curpressure = data.pressure
 
         if self.curpressure >= self.maxpressure then
             self.curpressure = self.maxpressure
             self.fullpressure = true
 
+            local newsection = self.GetPressureSection()
+
             if self.chargingdonefn then
                 self.chargingdonefn(self.inst)
             end
+
+		    if oldsection ~= newsection and self.pressuresectionfn then
+		        self.pressuresectionfn(newsection, oldsection, self.inst)
+		    end
 
             return true
         end
@@ -245,6 +252,11 @@ function SteamPressure:OnLoad(data)
 
         self.pressuretask = self.inst:DoTaskInTime(time, waitpressure, self)
 
+        local newsection = self.GetPressureSection()
+		if oldsection ~= newsection and self.pressuresectionfn then
+		    self.pressuresectionfn(newsection, oldsection, self.inst)
+		end
+
         if self.meterfn then
         	self.meterfn(self.inst)
         end
@@ -253,6 +265,7 @@ end
 
 function SteamPressure:LongUpdate(dt)
 	if self.pressuretask then
+		local oldsection = self.GetPressureSection()
 		self.pressuretask:Cancel()
 		self.pressuretask = nil
 		self.curpressure = self.maxpressure
@@ -265,9 +278,13 @@ function SteamPressure:LongUpdate(dt)
 				self.chargingdonefn(self.inst)
 			end
 		end
+		local newsection = self.GetPressureSection()
 		if self.meterfn then
     		self.meterfn(self.inst)
     	end
+		if oldsection ~= newsection and self.pressuresectionfn then
+		    self.pressuresectionfn(newsection, oldsection, self.inst)
+		end
 	end
 	dt = 0
 end
