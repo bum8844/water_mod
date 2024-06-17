78b0a622b0bec6a444af1515eefe95ef5fe77d3d
bum8844 <60079448+bum8844@users.noreply.github.com>
2024-06-17 20:21:40 +0900

    웬디 중국어 번역 백업

---
+++ scripts/languages/strings/zh/speech_wendy.lua
@@ -0,0 +1,357 @@
+return {
+    ACTIONFAIL =
+    {
+        GIVE =
+        {
+            WELL_NOTEMPTY = "我应该先拿掉那个桶。",
+            WELL_BUSY = "那个桶还在深渊里面。",
+        }
+    },
+
+        ANNOUNCE_DRUNK = "呜呜……阿比盖尔，我好孤独啊……",
+        ANNOUNCE_DRUNK_END = "别笑啊，阿比盖尔。很羞耻的。",
+        ANNOUNCE_DRUNK_IMMUNITY = "only_used_by_wathgrithr", -- 女武神专用，不用改
+        ANNOUNCE_NOTCHARGE = "看来它分享不了它的奶。",
+        ANNOUNCE_NOTHIRSTSLEEP = "我的喉咙正沉浸在干涸的深渊中，现在没法入睡。",
+        ANNOUNCE_NOTHIRSTSIESTA = "我的喉咙在阳光下干涸了，现在没法入睡。",
+        ANNOUNCE_SLEEP_END = "我怎么还在噩梦里？为什么这不是梦！？",
+        ANNOUNCE_THIRST = "我的喉咙好干啊。",
+
+        ANNOUNCE_CAFFINE_BUFF_START = "感觉身体变轻了。",
+        ANNOUNCE_CAFFINE_BUFF_STOP = "感觉身体变回以往的沉重了。",
+
+        ANNOUNCE_CURE_WATERBORNE = "刚才的痛苦平息了，但它不会是最后一个。",
+
+        ANNOUNCE_DCAPACITY_CRITICAL = "我可能会忘记那个羞耻感，阿比盖尔可不会。",
+        ANNOUNCE_DCAPACITY_HALF = "终于明白为什么我会这么羞耻了。",
+
+        DESCRIBE =
+        {
+            BOILBOOK = "就算拿到水解渴了，它还是会随着眼泪流出来。",
+
+            WATER_CLEAN_ICE = "和我的心一样冰冷。",
+            WATER_DIRTY_ICE = "冻起来的脏水。",
+
+            SPICE_CAFFEINPEPPER = "它比我更有用。",
+
+            WELL_KIT = "很重，不过我会努力的。",
+            WELL_SPRINKLER_KIT = "我只需要指定一个地方它就能把自己找到归宿。",
+            WELL_WATERPUMP_KIT = "冰冷的机械心脏。",
+            WELL_BURYING_KIT = "造成了这么深的空洞，就算掩盖住了也……",
+            WELL_DRILLING = "它在钻出一个很深的洞。",
+            WELL_DRILLING_ITEM = "它能在地上钻出一个很深的洞。",
+            HOLE = "还没掉到底怎么回家啊？", -- 提示：兔子先生在哪里啊
+            WELL = "如果我投个许愿币进去会怎样啊？",
+
+            WX78MODULE_NONEDRUNK = "它看起来很脆弱……希望别出悲剧性事故。",
+
+            -- 模组植物
+
+            TEA_TREE = "很翠绿的叶子。",
+            DUG_TEA_TREE = "它就这样逝去对我来说很好，虽然把它埋回土里比较好。",
+            TEA_SEED = "有个生命还在垂死挣扎。",
+            TEA_SEED_COOKED = "它在火海中逝去了。",
+            TEA_SEED_SAPLING = "相同的根，不同的茎。",
+            TEALEAVES = "这些叶子和茎的联系已经被斩断了。",
+            TEALEAVES_DRIED = "这些叶子被太阳无情地烧红了。",
+
+            CAFFEINBERRY = "一个能生成豆子的植物。",
+            DUG_CAFFEINBERRY = "没有家的你真可悲。",
+            CAFFEINBERRY_BEAN = "看起来有些许刺激性。",
+            CAFFEINBERRY_BEAN_COOKED = "没想到一颗豆子也能赋予毅力。",
+
+            RUINCACAO_TREE = "它把它的种子烧成了养分。",
+            DUG_RUINCACAO_TREE = "那个洞穴是属于你的地狱吗？",
+            RUINCACAO_SEED = "我把它从地狱之火拯救出来了。",
+            RUINCACAO_SEED_SAPLING = "有个生命在黑暗当中出现了。",
+            RUINCACAO = "困住渺小生命的躯壳。",
+            RUINCACAO_BEAN = "满满的油。",
+            RUINCACAO_BEAN_COOKED = "它们还是遭遇了既定的命运。",
+
+            -- 添加剂
+
+            ADDITIVE_SEED = "里面的种子有着不寻常的味道。",
+            ADDITIVE_PETALS = "干瘪的花瓣和叶子。",
+            ADDITIVE_NUT = "粉碎了的坚果。",
+            ADDITIVE_DAIRY = "奶油被封在里面。",
+
+            -- 水
+
+            WATER_CLEAN = "它可以暂时性延长生命。",
+            WATER_DIRTY = "想要持久的痛苦可以试试。",
+            WATER_SALTY = "它能孕育生命，也能作为毒药带来死亡。", -- 原文说是借鉴了Lord of the Flies
+
+            GOOPYDRINK = "和我的生命一样乱成一团糟。",
+
+            -- 茶壶饮料
+
+            FRUITJUICE = "用很多种水果做的。",
+            BERRIES_JUICE = "这些应该用了很多浆果吧。",
+            POMEGRANATE_JUICE = "洒了就会把你的衣服染成红色。",
+            BANANA_JUICE = "我能和阿比盖尔一起喝就好了……",
+            FIG_JUICE = "它的小种子卡在我的喉咙里了。",
+            DRAGONJUICE = "现在是果汁了。",
+            GLOWBERRYJUICE = "会让我的身体发出蓝光吗？",
+            WATERMELON_JUICE = "水水的。",
+
+            VEGGIE_TEA = "混了很多蔬菜泡出来的。",
+            CARROT_TEA = "用胡萝卜泡出来的水。",
+            CACTUS_TEA = "在水中溺死，真是讽刺。",
+            TOMATO_JUICE = "比起浓厚的血我更想要喝的是水。", -- 好像是某句歌词（From a Place of Love）
+            LUMPY_TEA = "它扎不了根了。",
+            SEAWEED_SHAKE = "我不能理解这个。",
+
+            GREENTEA = "这个能叫做茶吗？",
+            BLACKTEA = "为疲惫灵魂准备的短暂歇息……", -- 好像是小说来的（Pride and Prejudice）
+            BLACKTEA_ICED = "为疲惫灵魂准备的短暂歇息……",
+            FUER = "没看到帽子先生在开没人生日的茶会啊。", -- 爱丽丝梦游仙境的角色；有的是写“疯帽客”，不过考虑到英文的解释就不用了
+            MIXFLOWER = "我拿了一些花瓣泡出来的。",
+            HIBISCUSTEA = "酸酸的。",
+            CACTUSFLOWER_TEA = "泡在水里还是这么漂亮，真是令人妒忌。",
+            LOTUSTEA = "安宁在痛苦当中扎根了。",
+            SUSHIBISCUS = "为了和阿比盖尔在一起……",
+            MOON_BLOSSOM_TEA = "它的美貌已经达到极限了。",
+
+            CAFFEINBERRY_JUICE = "又黑又苦。",
+            TOFFEE_LATTE = "太甜了，咖啡的芳香也不在。",
+            RUINCHA_LATTE = "给不喜欢苦味的大人喝的咖啡。",
+
+            HOTRUINCOLATE = "好怀念啊。",
+            RUINCOLATE_SMOOTHIE = "甜味的漩涡。",
+            RUINXOCOLATL = "我和我的心刚才是不是滚下去了？",
+            TEA_RUINCOLATE_LATTE = "苦中带甜。",
+
+            MULLED = "它能应对冰冷且破碎的心吗？",
+            BUTTERBEER = "魔法般的甜味。",
+
+            -- 联动茶壶饮料
+
+            COCONUT_JUICE = "我的内心能像它那样硬就好了。",
+            RAINBOW_SHAKE = "装在杯里的美妙尸体。",
+            QUARTZ_SHAKE = "很适合小丑的形态。",
+            CHERRY_ROSEBERRIES_TEA = "要小心别把花苞吃下去了。",
+            CHERRY_HONEY_TEA = "还有比它更甜的吗？",
+            SWEET_RICE_DRINK = "里面有一些耐嚼的东西。",
+            PINEANANAS_JUICE = "它的果肉随着坚韧外皮被突破变成渣滓了。",
+            ORANGE_JUICE = "和它厚厚的外皮一样酸。",
+            STRAWBERRY_SMOOTHIE = "把草莓和冰块一起压碎的饮料。",
+            STRAWBERRY_MILK_SMOOTHIE = "溜嘴顺口。",
+
+            -- 其它
+
+            SPOILED_DRINK = "还能变得更糟糕吗？",
+
+            -- 气泡水
+
+            SODA = "好多气泡。",
+            FRUITSODA = "加了新鲜果汁的气泡水。",
+            LEMONLIMESODA = "甜得过火的气泡水。",
+            COLA = "征服了很多人的黑色甜水。",
+            COLAQUANTUM = "那个黑色的饮料甚至发光了。",
+            LUMPY_BEER = "这味道蛮适合我的。",
+
+            -- 酿酒
+
+            CORN_BEER = "很多气泡的苦酒。",
+            MADHU = "做这个偷了多少蜂蜜呢？",
+            WINE = "血红般的颜色。",
+            NOBLEWINE = "是用有白血病的水果做的吗？",
+            SPARKLINGWINE = "它的木塞在压制着里面的内容。",
+            GLOWBERRYWINE = "里面是不是有被困住的发光幽灵啊？",
+            KUMIS = "对婴儿来说可惜了。",
+            LUMPY_WINE = "透明中流露着像是芥菜的颜色。",
+            PULQUE = "那些傲慢的刺都消失了。",
+            RUINCACAO_WINE = "它不会是甜的了。",
+
+            -- 联动气泡水
+
+            CHERRY_DOCTOR_COLA = "它不是真的医生，不过应该可以给予一丝慰藉。",
+            PINEANANASSODA = "酸酸甜甜的气泡水。",
+            ORANGESODA = "加了鲜榨橘子汁的气泡水。",
+            STRAWBERRYSODA = "加了打碎了的草莓的气泡水。",
+
+            -- 联动酿酒
+
+            AUROBOW = "做成了酒还是那么色彩缤纷。",
+            COCONUT_WINE = "有点甜的气味。",
+            CHERRY_BLOOM_MADHU = "它还想要享受它那短暂的美吗？",
+            GIANT_BLUEBERRY_WINE = "有着爆炸性芳香的酒。",
+            RICE_WINE = "用白米酿出来的酒。",
+            WHEAT_BEER = "我尝不出它原本的甜味了。",
+            PALE_BEER = "真的好淡啊。",
+
+            -- 烈酒
+
+            DISINFECTANT = "它只会把伤口烧成荒地那样。",
+            CORN_WHISKEY = "只是个慢性的致命毒物。",
+            LUMPY_VODKA = "它让我的喉咙火辣辣的。",
+            TEQUILA = "口味粗糙的毒物。",
+            MADHU_RUM = "它会慢慢地沉浸在其中。",
+            BERRY_BRANDY = "它能让你忘记，包括忘记本身。",
+            AREUHI = "没有乳糖还是如此痛苦呢。",
+
+            -- 烈酒（添加剂）
+
+            BERRY_GIN = "没看过它作为药物派上用场。",
+            GLOWBERRY_GIN = "有着令人痴迷的蓝光。",
+            ABSINTHE = "像是穿着绿色衣服伪装成蟒蛇的女人。", -- 原文说是借鉴了Narnia
+            TEA_LUMPY_VODKA = "这不是健康饮料。",
+            PETALS_BERRY_BRANDY = "它的花香让人痴迷。",
+            KAHLUA = "闻起来很像浓缩咖啡。",
+            NUT_CORN_WHISKEY = "对于花生过敏的人来说是完美的。",
+            RUINCACAO_CREAM_LIQUEUR = "它只有气味是甜的。",
+            RUMCHATA = "让那些饱受折磨的孩子喝喝看。",
+
+            -- 联动烈酒
+
+            MOONWAY = "绽放着虹光的死亡。",
+            ARRACK = "蒸馏出来的棕榈酒。",
+            CHERRY_BLOOM_RUM = "看着它好过喝下去。",
+            RICE_SPIRITS = "像水的烈酒。",
+            WHEAT_WHISKEY = "只是个慢性的致命毒物。",
+            PALE_WHISKEY = "苍白骑士会注意着喝了这个的人。",
+
+            -- 联动烈酒（添加剂）
+
+            NUT_COCONUT_BRANDY = "同一个故事，不同的版本，都是真事……", -- 说是加勒比海盗（Tia Dalma）的情节
+            GIANT_BLUEBERRY_GIN = "它有着毁灭性的魅力。",
+
+            -- 烹饪锅
+
+            DARK_RUINCOLATE = "和我的生活一样又暗又苦。",
+            WHITE_RUINCOLATE = "我能和阿比盖尔一起吃就好了……",
+            RUINCOLATE = "可以吃的黑色燃料。",
+            RUIN_SCHOKAKOLA = "有着你要的醒脑条件。",
+
+            BUCKET_STEEL_EMPTY =
+            {
+                EMPTY = "坚韧中带有空心。",
+                FULL = "装满了雨水。",
+                FULL_ICE = "冻成块了。",
+                DIRTY = "有很多生命的毒水。",
+                DIRTY_ICE = "冻起来也不会变干净。",
+            },
+
+            BUCKET_WOODIE_EMPTY =
+            {
+                EMPTY = "被死亡掏空的一块木头。",
+                FULL = "装满了雨水。",
+                FULL_ICE = "冻成块了。",
+                DIRTY = "有很多生命的毒水。",
+                DIRTY_ICE = "冻起来也不会变干净。",
+            },
+
+            BUCKET_DRIFTWOOD_EMPTY = 
+            {
+                EMPTY = "用这个把骨灰装进去然后送入海里吧。",
+                FULL = "装满了雨水。", 
+                FULL_ICE = "冻成块了。", 
+                DIRTY = "有很多生命的毒水。", 
+                DIRTY_ICE = "冻起来也不会变干净。", 
+            },
+
+            BUCKET_EMPTY =
+            {
+                EMPTY = "只是个空木桶。",
+                FULL = "装满了雨水。",
+                FULL_ICE = "冻成块了。",
+                DIRTY = "有很多生命的毒水。",
+                DIRTY_ICE = "冻起来也不会变干净。",
+            },
+
+            DESALINATOR =
+            {
+                EMPTY = "只有空虚的声音。",
+                PURIFY_LONG = "才刚开始把盐过滤出来。",
+                PURIFY_SHORT = "水越来越清澈了。",
+                HASWATER = "干净的水好了。",
+                BURNT = "只剩下焦盐的味道。",
+                HASSALT = "就像是用盐过滤盐。",
+            },
+
+            BARREL =
+            {
+                GENERIC = "把水装进去好了。",
+                BURNT = "我的喉咙也会跟着烧起来。",
+            },
+
+            BREWERY =
+            {
+                EMPTY = "让材料腐烂后变成饮料的东西。",
+                BURNT = "只剩下烧焦的味道了。",
+                FERMENTING_LONG = "里面的东西应该还是新鲜的。",
+                FERMENTING_SHORT = "需要再多一点时间发酵。",
+                DONE = "又一个工作好了。",
+            },
+
+            CAMPKETTLE_ITEM = "有握柄的水壶。",
+            CAMPKETTLE =
+            {
+                GENERIC = "可以拿它装些湖水。",
+                BOILING_LONG = "这水才刚热起来。",
+                BOILING_SHORT = "需要再多一点时间煮开。",
+                DONE = "它可以撑多一段时间了。",
+                STOP = "还没完全煮开呢。",
+            },
+
+            CAMPDESALINATOR_ITEM = "只能勉强拿着它了。",
+            CAMPDESALINATOR =
+            {
+                GENERIC = "可以拿它装些海水。",
+                BOILING_LONG = "还有很多海水。",
+                BOILING_SHORT = "里面的多数生命没了吧。",
+                DONE = "它可以撑多一段时间了。",
+                STOP = "还没完全煮开呢。",
+            },
+
+            KETTLE =
+            {
+                EMPTY = "和我的眼睛一样干。",
+                BURNT = "烧焦了。",
+                BOILING_LONG = "里面的什么东西应该还在活蹦乱跳。",
+                BOILING_SHORT = "需要再多一点时间煮开。",
+                DONE = "终于可以解渴了。",
+                PURIFY_LONG = "这水才刚热起来。",
+                PURIFY_SHORT = "得先再煮多一会。",
+                HASWATER = "要去找些材料泡些饮料吗？",
+                MELT_LONG = "里面还是冻着的。",
+                MELT_SHORT = "再煮久一些让它化开。",
+            },
+
+            PORTABLEKETTLE_ITEM =
+            {
+                GENERIC = "为我干涸的喉咙准备的。",
+                EMPTY = "和我的眼睛一样干。",
+                BOILING_LONG = "里面的什么东西应该还在活蹦乱跳。",
+                BOILING_SHORT = "需要再多一点时间煮开。",
+                DONE = "终于可以解渴了。",
+                PURIFY_LONG = "这水才刚热起来。",
+                PURIFY_SHORT = "得先再煮多一会。",
+                HASWATER = "要去找些材料泡些饮料吗？",
+                MELT_LONG = "里面还是冻着的。",
+                MELT_SHORT = "再煮久一些让它化开。",
+            },
+
+            DISTILLERS =
+            {
+                EMPTY = "机器吞噬酒精，酒精吞噬人类。",
+                DISTILLTING_LONG = "应该会花好一些时间。",
+                DISTILLING_SHORT = "需要再等多一会。",
+                DONE = "又一个工作好了。",
+            },
+
+            WELL_WATERPUMP = {
+                HIGH_PRESSURE = "它在很活跃地抽水。",
+                MIDDLE_PRESSURE = "它会随着运动抽出水源。",
+                LOW_PRESSURE = "它像是心律失常了。",
+                RECHARG_PRESSURE = "它的心跳停止了。",
+            },
+
+            WELL_SPRINKLER =
+            {
+                LOWFUEL = "给它燃料就能继续工作。",
+                ON = "它抢了我的工作。",
+                OFF = "它停下来了。",
+            },
+        },
+}
