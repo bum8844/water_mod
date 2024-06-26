564b245a24f62ff1fe7637ac70774b97be47e8cb
AFS Co. Ltd. <qkrcotlr1@gmail.com>
2024-06-27 06:16:38 +0900

    improved string table structure

---
+++ scripts/languages/strings/common.lua
@@ -0,0 +1,366 @@
+return
+{
+	ACTIONS =
+	{
+		MILK = "우유 짜기",
+		BOIL = "끓이기",
+		DRINK = "마시기",
+		GIVEWATER = "물 넣기",
+		DRAIN = "비우기",
+		PURIFY = "정수하기",
+		MILKINGTOOL = "우유 짜기",
+		FERMENT = "발효시키기",
+		DISITLL = "증류하기",
+		BURYING = "매장하기",
+	},
+
+	NAMES =
+	{
+		WX78MODULE_NONEDRUNK = "에탄올처리 초확장모듈",
+
+
+		-- boilbook
+		BOILBOOK = "음료책",
+
+		-- well drilling
+		WELL_DRILLING ="우물드릴",
+		WELL_DRILLING_ITEM = "우물드릴",
+
+		-- wells & hole
+		HOLE = "우물구멍",
+		WELL = "우물",
+		WELL_SPRINKLER = "스프링클러",
+		WELL_KIT = "우물키트",
+		WELL_SPRINKLER_KIT = "스프링클러 키트",
+
+		--well_burying_kit
+		WELL_BURYING_KIT = "구멍매립키트",
+
+		-- desalinator
+		DESALINATOR = "제염기",
+
+		-- barrel
+		BARREL = "물통",
+
+		-- brewery
+		BREWERY = "양조기",
+
+		-- buckets
+		BUCKET_EMPTY = "양동이",
+		BUCKET_WOODIE_EMPTY = "속 빈 나무동이",
+		BUCKET_STEEL_EMPTY = "강철 양동이",
+		BUCKET_DRIFTWOOD_EMPTY = "유목동이",
+
+		-- campkettle
+		CAMPKETTLE_ITEM = "캠프주전자 키트",
+
+		CAMPKETTLE = "캠프주전자",
+
+		-- campcampdesalinator
+		CAMPDESALINATOR_ITEM = "캠프제염기 키트",
+		CAMPDESALINATOR = "캠프제염기",
+
+		-- kettle
+		KETTLE	= "주전자",
+
+		-- portablekettle
+		PORTABLEKETTLE_ITEM	= "휴대용 주전자",
+		PORTABLEKETTLE = "휴대용 주전자",
+
+		-- distillers
+		DISTILLERS = "증류기",
+
+		-- steam_waterpump
+		WELL_WATERPUMP = "스팀물펌프",
+		WELL_WATERPUMP_KIT = "스팀물펌프 키트",
+
+		-- tea_trees
+		TEA_TREE = "차나무",
+		DUG_TEA_TREE = "차나무" ,
+		TEA_SEED = "차나무 씨앗",
+		TEA_SEED_COOKED = "구운 차나무씨앗",
+		TEA_SEED_SAPLING = "차나무묘목",
+		TEALEAVES = "찻잎",
+		TEALEAVES_DRIED = "말린찻잎",
+
+		-- caffeinberry
+		DUG_CAFFEINBERRY = "커피식물",
+		CAFFEINBERRY = "커피덤불",
+		CAFFEINBERRY_BEAN = "커피열매",
+		CAFFEINBERRY_BEAN_COOKED = "구운 커피열매",
+		SPICE_CAFFEINPEPPER = "후추가루",
+
+		SPICE_CAFFEINPEPPER_FOOD = "후추뿌린 {food}",
+
+		-- ruincacao
+		RUINCACAO_TREE = "카카오나무",
+		DUG_RUINCACAO_TREE = "카카오나무" ,
+		RUINCACAO_SEED = "카카오나무 씨앗",
+		RUINCACAO_SEED_SAPLING = "카카오나무 묘목",
+		RUINCACAO = "카카오",
+		RUINCACAO_BEAN = "카카오콩",
+		RUINCACAO_BEAN_COOKED = "구운 카카오콩",
+
+		-- additives
+		ADDITIVE_SEED = "씨향료 첨가제",
+		ADDITIVE_PETALS = "엽향료 첨가제",
+		ADDITIVE_NUT = "넛향료 첨가제",
+		ADDITIVE_DAIRY = "유크림 첨가제",
+
+		-- preparedrink
+
+		WATER_CLEAN_ICE = "얼어붙은 물",
+		WATER_DIRTY_ICE = "더럽고 얼어붙은 물",
+		WATER_CLEAN = "물",
+		WATER_MINERAL = "물",
+		WATER_DIRTY = "더러운 물",
+		WATER_SALTY = "소금물",
+
+		GOOPYDRINK = "이상한 음료",
+
+		--과일쥬스
+
+		FRUITJUICE = "모듬 과일주스",
+		BERRIES_JUICE = "베리주스",
+		POMEGRANATE_JUICE = "석류주스",
+		BANANA_JUICE = "몽키 바나나",
+		FIG_JUICE = "무화과주스",
+		DRAGONJUICE = "용즙",
+		GLOWBERRYJUICE = "빛나는열매 주스",
+		WATERMELON_JUICE = "수박주스",
+
+		--야채쥬스
+
+		VEGGIE_TEA = "건강한 녹즙",
+		CARROT_TEA = "당근차",
+		CACTUS_TEA = "선인차",
+		LUMPY_TEA = "뿌리차",
+		SEAWEED_SHAKE = "해초쉐이크",
+		TOMATO_JUICE = "버진메리",
+
+		--엽차
+
+		GREENTEA = "녹차",
+		BLACKTEA = "홍차",
+		BLACKTEA_ICED = "아이스티",
+		FUER = "보이차",
+		MIXFLOWER = "가든블렌드티",
+		HIBISCUSTEA = "히비스커스티",
+		CACTUSFLOWER_TEA = "꽃선인차",
+		LOTUSTEA = "연꽃차",
+		SUSHIBISCUS = "히비스커스티?",
+		MOON_BLOSSOM_TEA = "달빛꽃차",
+
+		--커피,코코아,기타
+
+		CAFFEINBERRY_JUICE = "커피",
+		TOFFEE_LATTE = "토피넛라떼",
+		RUINCHA_LATTE = "모카라떼",
+
+		HOTRUINCOLATE = "핫초코",
+		RUINCOLATE_SMOOTHIE = "초코스무디",
+		RUINXOCOLATL = "쇼콜라틀",
+		TEA_RUINCOLATE_LATTE = "녹차초코라떼",
+
+		MULLED = "멀드와인",
+		BUTTERBEER = "버터맥주",
+
+		-- preparedrink 타모드 전용
+
+		COCONUT_JUICE = "후르츠 나타 데 코코",
+		RAINBOW_SHAKE = "무지개쉐이크",
+		QUARTZ_SHAKE = "쿼츠쉐이크",
+		CHERRY_ROSEBERRIES_TEA = "로즈힙티",
+		CHERRY_HONEY_TEA = "벚꽃꿀차",
+		SWEET_RICE_DRINK = "식혜",
+		PINEANANAS_JUICE = "파인애플주스",
+		ORANGE_JUICE = "오랜지주스",
+		STRAWBERRY_SMOOTHIE = "딸기스무디",
+		STRAWBERRY_MILK_SMOOTHIE = "딸기라떼스무디",
+
+		-- 유령차 유채이탈 관련 코드
+
+		GHOSTLY_TEA = "유령 차",
+
+		-- 사망 원인
+
+		DEHYDRATION = "탈수",
+		DEATH_BY_TEA = "유령 차",
+		WATERBORNEDEBUFF = "수인성질병",
+
+		-- prepareagedrink
+
+		SPOILED_DRINK = "썩은음료",
+
+		--소다
+
+		LUMPY_BEER = "루트비어",
+		SODA = "탄산수",
+		FRUITSODA = "과일소다",
+		LEMONLIMESODA = "허니소다",
+		COLA = "콜라",
+		COLAQUANTUM = "퀀텀콜라",
+
+		--양조주
+
+		CORN_BEER = "옥수수맥주",
+		MADHU = "벌꿀주",
+		WINE = "와인",
+		NOBLEWINE = "귀부와인",
+		SPARKLINGWINE = "스파클링와인",
+		GLOWBERRYWINE = "빛나는열매와인",
+		KUMIS = "산양유주",
+		LUMPY_WINE = "서주",
+		PULQUE = "풀케",
+		RUINCACAO_WINE = "카카오와인",
+
+		-- prepareagedrink 타모드 전용
+
+		--소다
+
+		CHERRY_DOCTOR_COLA = "닥터체리콜라",
+		PINEANANASSODA = "파인애플소다",
+		ORANGESODA = "오랜지소다",
+		STRAWBERRYSODA = "딸기소다",
+
+		--양조주
+
+		AUROBOW = "오로보우",
+		COCONUT_WINE = "코코넛와인",
+		CHERRY_BLOOM_MADHU = "벚꽃꿀주",
+		GIANT_BLUEBERRY_WINE = "붐베리와인",
+		RICE_WINE = "막걸리",
+		WHEAT_BEER = "밀맥주",
+		PALE_BEER = "페일에일",
+
+		-- preparedspiritsdrink
+
+		DISINFECTANT = "소독약",
+		CORN_WHISKEY = "옥수수위스키",
+		LUMPY_VODKA = "뿌리보드카",
+		TEQUILA = "테킬라",
+		MADHU_RUM = "벌꿀럼",
+		BERRY_BRANDY = "베리브렌디",
+		AREUHI = "아르히",
+
+		--왈리 전용
+
+		BERRY_GIN = "베리 진",
+		GLOWBERRY_GIN = "발광베리 진",
+		ABSINTHE = "압생트",
+		TEA_LUMPY_VODKA = "녹차리큐르",
+		PETALS_BERRY_BRANDY = "로즈브랜디",
+		KAHLUA = "커피리큐르",
+		NUT_CORN_WHISKEY = "너트위스키",
+		RUINCACAO_CREAM_LIQUEUR = "카카오 크림 데 리큐르",
+		RUMCHATA = "럼차타",
+
+		-- preparedspiritsdrink 타모드 전용
+
+		MOONWAY = "문웨이",
+		ARRACK = "아락",
+		CHERRY_BLOOM_RUM = "벚꽃꿀 럼주",
+		RICE_SPIRITS = "청주",
+		WHEAT_WHISKEY = "밀위스키",
+		PALE_WHISKEY = "페일몰트 위스키",
+
+		--왈리 전용 타모드 전용
+
+		NUT_COCONUT_BRANDY = "카리브브랜디",
+		GIANT_BLUEBERRY_GIN = "붐!베리사파이어",
+
+		--냄비 요리
+
+		DARK_RUINCOLATE = "다크초콜릿",
+		WHITE_RUINCOLATE = "화이트초콜릿",
+		RUINCOLATE = "초콜릿",
+		RUIN_SCHOKAKOLA = "쇼카콜라",
+	},
+
+	RECIPE_DESC =
+	{
+		WX78MODULE_NONEDRUNK = "취할틈도없이 에탄올을 처리합니다.",
+		BOILBOOK = "당신의 모든음료제조법을 복습하세요." ,
+		WELL_DRILLING_ITEM = "땅에다가 엄청 큰 구멍을 뚫어보세요.",
+		WELL_KIT = "우물가에선 우물쭈물거릴거 없습니다.",
+		WELL_SPRINKLER_KIT = "궁극의 관개농업.",
+		WELL_BURYING_KIT = "구멍에 행운의 동전대신 돌을.",
+		DESALINATOR = "짠돌이 바닷물을 덜 짜게.",
+		BARREL = "원숭이 미포함.",
+		BREWERY = "맛있는음료를 만들수 있을...까요?",
+		BUCKET_EMPTY = "비어있는양동이.",
+		CAMPKETTLE_ITEM = "아마도 여러분의 첫 주전자.",
+		CAMPDESALINATOR_ITEM = "아마도 여러분의 첫 제염기.",
+		KETTLE = "차 한잔 하시죠.",
+		PORTABLEKETTLE_ITEM = "티타임을 언제,어디서나.",
+		DISTILLERS = "술향을 농축할수 있을까요?",
+		WELL_WATERPUMP_KIT = "압력으로 깨끗한물을 뽑으세요!",
+		SPICE_CAFFEINPEPPER = "기운넘치는 활력의 가루.",
+		ADDITIVE_SEED = "여러씨앗이 섞여있습니다.",
+		ADDITIVE_PETALS = "이파리와 꽃잎의 모음집.",
+		ADDITIVE_NUT = "열매들을 모아놓은 봉지.",
+		ADDITIVE_DAIRY = "고소한향을 내고싶다면.",
+	},
+
+	UI =
+	{
+		CRAFTING_FILTERS =
+		{
+			HYDRATION = "물과 음료",
+			SECRETKNOWLEDGE = "비밀 지식",
+		},
+
+		DRINK_TYPES =
+		{
+			GENERIC = "음료",
+			FRUIT = "과일",
+			VEGGIE = "채소",
+			LEAFS = "잎류",
+			BREWER = "양조주",
+			SPIRITS = "증류주",
+			MEDICINE = "의약품",
+		},
+
+		COOKBOOK =
+		{
+			TAB_TITLE_KETTLE = "주전자 레시피",
+			TAB_TITLE_BREWERY = "양조기 레시피",
+			TAB_TITLE_DISTILLERS = "증류기 레시피",
+			BOILTIME_TITLE = "끓는 시간",
+			SORT_THIRST = "수분",
+			FILTER_FRUIT = "과일류",
+			FILTER_LEAFS = "잎종류",
+			FOOD_EFFECTS_CAFFINE_SLEEP_RESIST = "이동속도 증가,각성제",
+			FOOD_EFFECTS_CURE_WATERBORNE = "수인성 질병 치료",
+			FOOD_EFFECTS_DETOX = "해장 효과",
+			FOOD_EFFECTS_MULLED = "강한 해장 효과",
+			FOOD_EFFECTS_CAFFINE = "이동속도 증가",
+			FOOD_EFFECTS_COLA = "최고의 음료",
+			FOOD_EFFECTS_COLAQUANTUM = "모두가사랑하는음료",
+			FOOD_EFFECTS_HAYFEVER_RELIEF_MED = "건초열완화",
+			FOOD_EFFECTS_SUS = "수상할 정도로 수상함",
+			FOOD_EFFECTS_BUTTERHUNTER = "버터를 더 얻을수있음",
+			FOOD_EFFECTS_ANCIENT_KNOWLEGEE_PIECE = "불완전한유적물품 한번 제작가능",
+			FOOD_EFFECTS_ANCIENT_KNOWLEGEE = "유적물품 한번 제작가능",
+			FOOD_EFFECTS_ANCIENT_KNOWLEGEE_THIRD = "유적물품 세번 제작가능",
+			FOOD_EFFECTS_CONSERVE_KNOWLEGEE = "백과지식을 늘려줌",
+			FOOD_EFFECTS_CONSERVE_KNOWLEGEE_HUGE = "백과지식을 많이늘려줌",
+			FOOD_EFFECTS_CONSERVE_KNOWLEGEE_CAFFINE = "이속증가,백과지식을 늘려줌",
+			FOOD_EFFECTS_NAG_AURA_RESIST = "정신력감소저항",
+			FOOD_EFFECTS_NAG_AURA_RESIST_GLOW = "정신력감소저항 및 발광",
+			FOOD_EFFECTS_NAG_AURA_RESIST_ELECTRIC_ATTACK = "정신력감소저항 및 전기공격",
+			FOOD_EFFECTS_NAG_AURA_RESIST_CHERRY_TAFFY = "정신력감소저항 및 하루동안 체력증가",
+			FOOD_EFFECTS_NAG_AURA_RESIST_HAYFEVER_RELIEF_HUGE = "정신력감소저항 및 건초열완화 강화",
+			FOOD_EFFECTS_NAG_AURA_RESIST_ANCIENT_KNOWLEGEE = "정신력감소저항 및 유적물품 한번 제작가능",
+			FOOD_EFFECTS_NAG_AURA_RESIST_DRY = "정신력감소저항 및 습기제거",
+			FOOD_EFFECTS_INTOXICATION = "만취 및 피해경감",
+			FOOD_EFFECTS_INTOXICATION_GLOW = "만취,피해경감 및 발광",
+			FOOD_EFFECTS_INTOXICATION_ELECTRIC_ATTACK = "만취,피해경감 및 전기공격",
+			FOOD_EFFECTS_INTOXICATION_CHERRY_TAFFY = "만취,피해경감 및 하루동안 체력증가",
+			FOOD_EFFECTS_INTOXICATION_HAYFEVER_RELIEF_HUGE = "만취,피해경감 및 건초열면역",
+			FOOD_EFFECTS_INTOXICATION_CAFFINE = "만취,피해경감 및 이속증가",
+			FOOD_EFFECTS_INTOXICATION_ANCIENT_KNOWLEGEE_THIRD = "만취,피해경감 및 유적물품 세번 제작가능",
+			FOOD_EFFECTS_INTOXICATION_DRY = "만취,피해경감 및 방습",
+		},
+	},
+}
---
+++ scripts/languages/strings/showme.lua
@@ -0,0 +1,25 @@
+return
+{
+	SHOWME =
+	{
+		BOILWARE =
+		{
+			PRODUCT = "예상되는 음료 %s\n",
+			TIME = "다되기 까지: %s\n",
+			AMOUNT = "물: %s/%s\n",
+			SALT = "소금: %s\n",
+		},
+
+		THIRSTVALUE =
+		{
+			THIRST = "목마름: %s\n",
+		},
+
+		WATERCOLLATOR =
+		{
+			STEAM = "증기: %s\n",
+			WAITRAIN = "비오기 기다리는중\n",
+			AMOUNT = "물: %s\n",
+		},
+	},
+}
